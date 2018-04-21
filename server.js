0d44d971428fa9ecdadfe084801a03930f959040
jm <jm@jmdeMac-mini.local>
2018-04-21 22:05:21 +0800

    add check

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ OAuth._stateFromQuery = function (query) {
 Meteor.methods({
   'zhaoyao91:short-oauth-state.setState'(id, state) {
     check(id, String);
-    check(state, [Match.Any]);
+    check(state, Match.Any);
     const expiredAt = getExpiredAt()
     State.upsert({_id: id}, {$set: {state, expiredAt}})
     return id
